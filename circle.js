c82def9be5f42ab8c7b3fda37a567ded5ce5cfe7
sevvalkanat <84670473+sevvalkanat@users.noreply.github.com>
2022-03-18 13:51:43 +0300

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,8 @@
+function circleArea (R) {
+    console.log(Math.PI*(R*R));
+}
+function circleCircumference (R){
+    console.log(Math.PI*(2 * R))
+}
+module.exports = {circleArea,circleCircumference};
+ 
